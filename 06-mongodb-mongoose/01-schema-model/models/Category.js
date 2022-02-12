02a1b0a8f9ab5b20f3d107942c4c81ceb3e4ed36
AndrewPetin <petrucchioandrew@gmail.com>
2022-02-12 14:48:31 +0100

    add solution for 6-module 1-task
    Model's schema

--- 06-mongodb-mongoose/01-schema-model/models/Category.js
+++ 06-mongodb-mongoose/01-schema-model/models/Category.js
@@ -1,10 +1,22 @@
 const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
-const subCategorySchema = new mongoose.Schema({
-});
+const basicSchemaDescription = {
+  title: {
+    type: String,
+    required: true,
+  },
+};
+
+const subCategorySchema = new mongoose.Schema({...basicSchemaDescription});
 
 const categorySchema = new mongoose.Schema({
+  ...basicSchemaDescription,
+  subcategories: [subCategorySchema],
+  // subcategories: [{
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: 'Subcategory',
+  // }],
 });
 
 module.exports = connection.model('Category', categorySchema);
