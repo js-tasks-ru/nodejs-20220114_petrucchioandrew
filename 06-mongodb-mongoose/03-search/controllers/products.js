98d672844c670df695d3106edd85402ca7472433
AndrewPetin <petrucchioandrew@gmail.com>
2022-02-13 15:27:21 +0100

    add solution for 6-module 3-task
    search compound index

--- 06-mongodb-mongoose/03-search/controllers/products.js
+++ 06-mongodb-mongoose/03-search/controllers/products.js
@@ -1,3 +1,12 @@
+const Product = require('../models/Product');
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  const {query} = ctx.query;
+  const products = await Product.find({
+    $text: {$search: query},
+  }, {
+    score: {$meta: 'textScore'},
+  }).sort( {score: {$meta: 'textScore'}} );
+
+  ctx.body = {products};
 };
