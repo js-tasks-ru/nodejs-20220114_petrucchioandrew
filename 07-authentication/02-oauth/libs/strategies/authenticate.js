04bfec66b6401baf7791e6e7db6bd2e2c0e1b602
AndrewPetin <petrucchioandrew@gmail.com>
2022-02-18 16:49:46 +0100

    add solution for 7-module 2-task
    oauth2 protocol

--- 07-authentication/02-oauth/libs/strategies/authenticate.js
+++ 07-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,3 +1,22 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  if (!email) {
+    return done(null, false, `Не указан email`);
+  }
+
+  let user = await User.findOne({email});
+
+  if (user) {
+    return done(null, user);
+  } else {
+    user = await new User({email, displayName})
+      .save()
+      .then(user => {
+        done(null, user);
+      })
+      .catch(err => {
+        done(err);
+      });
+  }
 };
